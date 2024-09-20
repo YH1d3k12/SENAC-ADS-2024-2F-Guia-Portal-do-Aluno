989f380fe25802de35e8dbd7ab0c04471fe4374d
Djalma <djalmahy@hotmail.com>
2024-09-19 21:43:58 -0300

    Feat: Main Page

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import "../styles/footer.css";
+
+
+export default function Footer() {
+    return (
+        <footer className="footer">
+            <p>Desenvolvido por: Djalma Hideki Yamamoto</p>
+        </footer>
+    );
+};
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import logo from "../assets/logo.png"
+import "../styles/header.css";
+
+export default function Header() {
+    return (
+        <header className="header">
+            <nav className="navbar">
+                <h1>SENAC</h1>
+                <p>Panfleto Informativo</p>
+            </nav>
+        </header>
+    );
+};
